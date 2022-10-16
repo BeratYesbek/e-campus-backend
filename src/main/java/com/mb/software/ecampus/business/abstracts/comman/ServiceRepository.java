efa4a20326ef8759bd165e1e9bdad202b51efa4e
Feanor <beratyesbek@gmail.com>
2022-10-16 11:18:31 +0300

    ServiceRepository has been created

--- src/main/java/com/mb/software/ecampus/business/abstracts/comman/ServiceRepository.java
+++ src/main/java/com/mb/software/ecampus/business/abstracts/comman/ServiceRepository.java
@@ -1,7 +1,14 @@
 package com.mb.software.ecampus.business.abstracts.comman;
 
+import com.mb.software.ecampus.core.utilities.Result;
 import com.mb.software.ecampus.core.utilities.data.DataResult;
 
-public interface ServiceRepository<T> {
+import java.util.List;
 
+public interface ServiceRepository<T> {
+    DataResult<T> add(T entity);
+    DataResult<T> update(T entity);
+    Result delete(int id);
+    DataResult<T> getById(int id);
+    DataResult<List<T>> getAll();
 }
