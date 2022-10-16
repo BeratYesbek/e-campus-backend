4afaa7e7662de6fb5044dc941971e256c9e322ab
Feanor <beratyesbek@gmail.com>
2022-10-16 18:40:10 +0300

    CRUD methods have been created for AcademicUnitsController

--- src/main/java/com/mb/software/ecampus/api/controllers/AcademicUnitsController.java
+++ src/main/java/com/mb/software/ecampus/api/controllers/AcademicUnitsController.java
@@ -1,14 +1,14 @@
 package com.mb.software.ecampus.api.controllers;
 
 import com.mb.software.ecampus.business.abstracts.AcademicUnitService;
+import com.mb.software.ecampus.core.utilities.Result;
 import com.mb.software.ecampus.core.utilities.data.DataResult;
 import com.mb.software.ecampus.entities.concretes.AcademicUnit;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/academic_units")
@@ -20,7 +20,27 @@ public class AcademicUnitsController {
     }
 
     @PostMapping
-    private ResponseEntity<DataResult<AcademicUnit>>  add(@RequestBody AcademicUnit academicUnit) {
-        return new ResponseEntity<>(academicUnitService.add(academicUnit),HttpStatus.OK);
+    private ResponseEntity<DataResult<AcademicUnit>> add(@RequestBody AcademicUnit academicUnit) {
+        return new ResponseEntity<>(academicUnitService.add(academicUnit), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<DataResult<AcademicUnit>> update(@PathVariable int id, @RequestBody AcademicUnit academicUnit) {
+        return new ResponseEntity<>(academicUnitService.update(academicUnit), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Result> delete(@PathVariable int id) {
+        return new ResponseEntity<>(academicUnitService.delete(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<DataResult<AcademicUnit>> getById(@PathVariable int id) {
+        return new ResponseEntity<>(academicUnitService.getById(id), HttpStatus.OK);
+    }
+
+    @GetMapping
+    private ResponseEntity<DataResult<List<AcademicUnit>>> getAll() {
+        return new ResponseEntity<>(academicUnitService.getAll(), HttpStatus.OK);
     }
 }
