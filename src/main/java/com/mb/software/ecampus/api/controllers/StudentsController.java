cdddef13bdfdc47cccfc0f7eeb36b945595ab99b
Feanor <beratyesbek@gmail.com>
2022-10-21 21:07:15 +0300

    closes #3; API methods created for StudentsController

--- src/main/java/com/mb/software/ecampus/api/controllers/StudentsController.java
+++ src/main/java/com/mb/software/ecampus/api/controllers/StudentsController.java
@@ -1,13 +1,15 @@
 package com.mb.software.ecampus.api.controllers;
 
 import com.mb.software.ecampus.business.abstracts.StudentService;
+import com.mb.software.ecampus.core.utilities.Result;
 import com.mb.software.ecampus.core.utilities.data.DataResult;
 import com.mb.software.ecampus.entities.concretes.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.crypto.Data;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
@@ -27,4 +29,42 @@ public class StudentsController {
         }
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<DataResult<Student>> update(@PathVariable int id, @RequestBody Student student) {
+        DataResult result = studentService.update(student);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+
+    @DeleteMapping("/{id}")
+    private ResponseEntity<Result> delete(@PathVariable int id) {
+        Result result = studentService.delete(id);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/{id}")
+    private ResponseEntity<DataResult<Student>> getById(@PathVariable int id) {
+        DataResult result = studentService.getById(id);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping
+    private ResponseEntity<DataResult<List<Student>>> getAll() {
+        DataResult result = studentService.getAll();
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
