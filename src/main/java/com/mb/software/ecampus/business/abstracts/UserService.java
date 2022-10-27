f91ed9cd78b64a7cba98c2de1ee273e29dc12245
Feanor <beratyesbek@gmail.com>
2022-10-27 10:37:42 +0300

    findByEmail method added to UserService

--- src/main/java/com/mb/software/ecampus/business/abstracts/UserService.java
+++ src/main/java/com/mb/software/ecampus/business/abstracts/UserService.java
@@ -1,7 +1,10 @@
 package com.mb.software.ecampus.business.abstracts;
 
 import com.mb.software.ecampus.business.abstracts.common.ServiceRepository;
+import com.mb.software.ecampus.core.utilities.results.data.DataResult;
 import com.mb.software.ecampus.entities.concretes.User;
 
 public interface UserService extends ServiceRepository<User> {
+
+    DataResult<User> getByEmail(String email);
 }
--- src/main/java/com/mb/software/ecampus/dataAccess/dao/UserDao.java
+++ src/main/java/com/mb/software/ecampus/dataAccess/dao/UserDao.java
@@ -4,4 +4,5 @@ import com.mb.software.ecampus.entities.concretes.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDao extends JpaRepository<User, Integer> {
+     User finByEmail(String email);
 }
