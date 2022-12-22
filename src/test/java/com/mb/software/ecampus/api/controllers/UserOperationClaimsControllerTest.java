ba5c58e3399d1e71ec19376d131ec1db4d56a5ff
seheerst <seherst9@gmail.com>
2022-12-22 15:32:40 +0300

    feat: UserOperationClaimsControllerTest complated

---
+++ src/test/java/com/mb/software/ecampus/api/controllers/UserOperationClaimsControllerTest.java
@@ -0,0 +1,46 @@
+package com.mb.software.ecampus.api.controllers;
+
+import static org.mockito.Mockito.*;
+
+import com.mb.software.ecampus.business.abstracts.UserOperationClaimService;
+import com.mb.software.ecampus.core.entities.OperationClaim;
+import com.mb.software.ecampus.core.entities.User;
+import com.mb.software.ecampus.core.entities.UserOperationClaim;
+import com.mb.software.ecampus.core.utilities.results.data.DataResult;
+import com.mb.software.ecampus.core.utilities.results.data.SuccessDataResult;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+@ExtendWith(SpringExtension.class)
+public class UserOperationClaimsControllerTest {
+    @Mock
+    private UserOperationClaimService service;
+
+    @InjectMocks
+    private UserOperationClaimsController userOperationClaimsController;
+
+    @Test
+    public void testGetByIdIsWorkingProperly(){
+        when(service.getByUserId(userOperationClaimId)).thenReturn(prepareDataList());
+        ResponseEntity<DataResult<List<UserOperationClaim>>> response = userOperationClaimsController.getByUserId(userOperationClaimId);
+        assertEquals(response.getStatusCode(),HttpStatus.OK);
+        assertEquals(response.getBody().getData(),prepareDataList().getData());
+    }
+
+    private DataResult<List<UserOperationClaim>> prepareDataList() {
+        List<UserOperationClaim> operationClaims = new ArrayList<>();
+        operationClaims.add(1,new UserOperationClaim());
+        return new SuccessDataResult<>(operationClaims);
+    }
+
+    private static final int userOperationClaimId = 0;
+}
