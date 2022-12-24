34868ced61941cbe7c4e51487e4281477d8e5275
MDD <mdede.i@outlook.com>
2022-12-24 17:22:36 +0300

    LessonContent entity, dao, service and controller added

--- src/main/java/com/mb/software/ecampus/api/controllers/LessonContentsController.java
+++ src/main/java/com/mb/software/ecampus/api/controllers/LessonContentsController.java
@@ -1,9 +1,9 @@
 package com.mb.software.ecampus.api.controllers;
 
-import com.mb.software.ecampus.business.abstracts.StudentService;
+import com.mb.software.ecampus.business.abstracts.LessonContentService;
 import com.mb.software.ecampus.core.utilities.results.Result;
 import com.mb.software.ecampus.core.utilities.results.data.DataResult;
-import com.mb.software.ecampus.entities.concretes.Student;
+import com.mb.software.ecampus.entities.concretes.LessonContent;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,45 +11,45 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/students")
-public class StudentsController {
+@RequestMapping("api/lesson_contents")
+public class LessonContentsController {
 
-    private final StudentService studentService;
+    private final LessonContentService lessonContentService;
 
-    public StudentsController(StudentService studentService) {
-        this.studentService = studentService;
+    public LessonContentsController(LessonContentService lessonContentService) {
+        this.lessonContentService = lessonContentService;
     }
 
     @PostMapping
-    public ResponseEntity<DataResult<Student>> add(@RequestBody Student student) throws Exception {
-        DataResult result = studentService.add(student);
-        if (result.isSuccess()) {
+    public ResponseEntity<DataResult<LessonContent>> add(@RequestBody LessonContent lessonContent) throws Exception{
+        DataResult result = lessonContentService.add(lessonContent);
+        if (result.isSuccess()){
             return new ResponseEntity<>(result, HttpStatus.CREATED);
         }
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<DataResult<Student>> update(@PathVariable int id, @RequestBody Student student) {
-        DataResult result = studentService.update(student);
-        if (result.isSuccess()) {
+    public ResponseEntity<DataResult<LessonContent>> update(@PathVariable int id, @RequestBody LessonContent lessonContent){
+        DataResult result = lessonContentService.update(lessonContent);
+        if (result.isSuccess()){
             return new ResponseEntity<>(result, HttpStatus.OK);
         }
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Result> delete(@PathVariable int id) {
-        Result result = studentService.delete(id);
-        if (result.isSuccess()) {
+    public ResponseEntity<Result> delete(@PathVariable int id){
+        Result result = lessonContentService.delete(id);
+        if (result.isSuccess()){
             return new ResponseEntity<>(result, HttpStatus.OK);
         }
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<DataResult<Student>> getById(@PathVariable int id) {
-        DataResult result = studentService.getById(id);
+    public ResponseEntity<DataResult<LessonContent>> getById(@PathVariable int id){
+        DataResult result = lessonContentService.getById(id);
         if (result.isSuccess()) {
             return new ResponseEntity<>(result, HttpStatus.OK);
         }
@@ -57,13 +57,12 @@ public class StudentsController {
     }
 
     @GetMapping
-    public ResponseEntity<DataResult<List<Student>>> getAll() {
-        DataResult result = studentService.getAll();
+    public ResponseEntity<DataResult<List<LessonContent>>> getAll(){
+        DataResult result = lessonContentService.getAll();
         if (result.isSuccess()) {
             return new ResponseEntity<>(result, HttpStatus.OK);
         }
         return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
     }
-
 
 }
