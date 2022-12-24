258fac7015303cc17c7bafab6916528f9315f8ac
seheerst <seherst9@gmail.com>
2022-12-24 20:48:17 +0300

    feat: AcademicUnitsControllerTest updated

--- src/main/java/com/mb/software/ecampus/core/business/concretes/AcademicUnitServiceImpl.java
+++ src/main/java/com/mb/software/ecampus/core/business/concretes/AcademicUnitServiceImpl.java
@@ -1,6 +1,6 @@
-package com.mb.software.ecampus.business.concretes;
+package com.mb.software.ecampus.core.business.concretes;
 
-import com.mb.software.ecampus.business.abstracts.AcademicUnitService;
+import com.mb.software.ecampus.core.business.abstracts.AcademicUnitService;
 import com.mb.software.ecampus.core.utilities.results.Result;
 import com.mb.software.ecampus.core.utilities.results.SuccessResult;
 import com.mb.software.ecampus.core.utilities.results.data.DataResult;
