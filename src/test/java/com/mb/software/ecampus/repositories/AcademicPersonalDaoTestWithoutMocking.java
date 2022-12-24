7a53d20c17efc6589942249f366986e1b51c37e5
shmsacr <shmsacr@gmail.com>
2022-12-24 12:05:24 +0300

    feat:  add Academic personal da test without mocking page librays

--- src/test/java/com/mb/software/ecampus/repositories/AcademicPersonalDaoTestWithoutMocking.java
+++ src/test/java/com/mb/software/ecampus/repositories/AcademicPersonalDaoTestWithoutMocking.java
@@ -1,4 +1,17 @@
 package com.mb.software.ecampus.repositories;
 
+import com.mb.software.ecampus.dataAccess.dao.AcademicUnitDao;
+import com.mb.software.ecampus.entities.concretes.AcademicUnit;
+import com.mb.software.ecampus.entities.concretes.enums.AcademicType;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class AcademicPersonalDaoTestWithoutMocking {
 }
