989f4e6a103db9be966e8884135943f8cc6167ba
shmsacr <shmsacr@gmail.com>
2022-12-24 13:17:02 +0300

    feat: Academic personal da test without mocking completed save method

--- src/test/java/com/mb/software/ecampus/repositories/AcademicPersonalDaoTestWithoutMocking.java
+++ src/test/java/com/mb/software/ecampus/repositories/AcademicPersonalDaoTestWithoutMocking.java
@@ -1,7 +1,13 @@
 package com.mb.software.ecampus.repositories;
 
+import com.mb.software.ecampus.core.entities.User;
+import com.mb.software.ecampus.dataAccess.dao.AcademicPersonalDao;
 import com.mb.software.ecampus.dataAccess.dao.AcademicUnitDao;
+import com.mb.software.ecampus.entities.concretes.AcademicPersonal;
 import com.mb.software.ecampus.entities.concretes.AcademicUnit;
+import com.mb.software.ecampus.entities.concretes.Employee;
+import com.mb.software.ecampus.entities.concretes.EmployeeDepartment;
+import com.mb.software.ecampus.entities.concretes.enums.AcademicRate;
 import com.mb.software.ecampus.entities.concretes.enums.AcademicType;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +15,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class AcademicPersonalDaoTestWithoutMocking {
+
+
+    @Autowired
+    public AcademicPersonalDao academicPersonalDao;
+
+    @Test
+    public  void testSave(){
+        AcademicPersonal academicPersonal = new AcademicPersonal(2, AcademicRate.PROFESSOR,
+                new Employee(1,
+                        new EmployeeDepartment(1,"Sehmus"),
+                        new AcademicUnit(1, "Engineering Faculty",
+                                AcademicType.FACULTY),new User(1,"Sehmus","test@gmail.com",
+                        new Date(2000, Calendar.DECEMBER,5),
+                        new Date(2022, Calendar.SEPTEMBER,5,12,45),"123456")));
+        AcademicPersonal addesAcademicPersonal = academicPersonalDao.save(academicPersonal);
+        assertEquals(academicPersonal,addesAcademicPersonal);
+    }
+
 }
