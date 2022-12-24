c3d082fdfbe5520d1a727e0fd8a8206eddd8e147
seheerst <seherst9@gmail.com>
2022-12-24 20:49:34 +0300

    feat: LessonsControllerTest updated

--- src/main/java/com/mb/software/ecampus/business/abstracts/LessonService.java
+++
@@ -1,8 +0,0 @@
-package com.mb.software.ecampus.business.abstracts;
-
-import com.mb.software.ecampus.business.abstracts.common.ServiceRepository;
-import com.mb.software.ecampus.entities.concretes.Lesson;
-
-
-public interface LessonService extends ServiceRepository<Lesson> {
-}
